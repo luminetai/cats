efb57191932e07e127257ce9b338070c8a671f00
Denis Ustinov <83418606+DenisUstinov@users.noreply.github.com>
2025-03-14 07:25:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,43 +1,38 @@
 #!/bin/bash
 
-# Функция для установки зависимостей в зависимости от операционной системы
+# Скачать и установить зависимости
 install_dependencies() {
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Для Linux устанавливаем xclip
-    echo "Linux detected. Installing xclip..."
+    # Установка xclip для Linux
+    echo "Installing xclip on Linux..."
     sudo apt-get update
     sudo apt-get install -y xclip
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # Для macOS устанавливаем pbcopy
-    echo "macOS detected. Installing pbcopy..."
+    # Установка pbcopy для macOS
+    echo "Installing pbcopy on macOS..."
     brew install pbcopy
   else
-    echo "Error: Unsupported OS. Only Linux and macOS are supported."
+    echo "Unsupported OS. Only Linux and macOS are supported."
     exit 1
   fi
 }
 
-# Функция для установки скрипта cats
+# Установка скрипта cats
 install_cats() {
   echo "Installing cats script..."
-
-  # Скачиваем cats.sh в /usr/local/bin
   sudo curl -fsSL https://raw.githubusercontent.com/luminetai/cats/main/cats.sh -o /usr/local/bin/cats
-
-  # Делаем файл исполнимым
   sudo chmod +x /usr/local/bin/cats
-
-  echo "cats installed successfully. You can now use the 'cats' command."
+  echo "cats successfully installed!"
 }
 
-# Основная функция установки
+# Основная установка
 main_install() {
-  # Устанавливаем зависимости
+  # Установка зависимостей
   install_dependencies
-
-  # Устанавливаем cats
+  
+  # Установка cats
   install_cats
 }
 
-# Запуск основной функции
+# Выполнение всего процесса
 main_install
